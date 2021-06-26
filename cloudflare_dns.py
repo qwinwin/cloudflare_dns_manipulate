0eb6cb24bf1efe9662023b292ea70fecfcdaa5da
qwinwin <qwinwin7@gmail.com>
2021-06-27 01:20:15 +0800

    replace logging with loguru && add support for mulitiple records in the same domain

--- cloudflare_dns.py
+++ cloudflare_dns.py
@@ -2,7 +2,7 @@
 # coding = utf-8
 import httpx
 import argparse
-import logging
+from loguru import logger
 
 cf_id = "cloudflare account id(mail)"
 cf_key = "global api key"
@@ -11,36 +11,44 @@ zone_name = 'domain zone name'
 
 
 def list_record(name=''):
-    global record
-    params = {'name': f'{name}.{zone_name}'} if name else {'per_page': 100}
+    req_name = f'{name}.{zone_name}'
+    params = {'name': f'{req_name}'} if name else {'per_page': 100}
     resp = httpx.get(base_url, headers=headers, params=params)
+    msgs = []
+    count = 0
     for msg in resp.json()['result']:
-        print(eval(req_msg))
-        if name:
-            record = eval(req_msg)
-            return msg['id']
+        if not name:
+            print(eval(req_msg))
+        if msg['name'] == f'{req_name}':
+            msgs.append(msg)
+            print(f"{count}: {eval(req_msg)}")
+            count += 1
+    return msgs
 
 
-def new_record(name, content, record_id, record_type='A'):
+def new_record(name, content, msgs, record_type='A'):
     data = {'type': record_type, 'name': name, 'content': content}
-    if record_id:
-        msg = httpx.put(f'{base_url}/{record_id}', headers=headers,
+    if msgs and not multi:
+        option = int(input("choose the option: ")) if len(msgs) > 1 else 0
+        msg = msgs[option]
+        old_msg = eval(req_msg)
+        msg = httpx.put(f'{base_url}/{msg["id"]}', headers=headers,
                         json=data).json()['result']
-        print('Changed to')
-        logging.info(f'update: {eval(req_msg)}')
+        logger.info(f"CHANGED | {old_msg} -> {eval(req_msg)}")
     else:
         msg = httpx.post(base_url, headers=headers, json=data).json()['result']
+        logger.info(f"NEW | {eval(req_msg)}")
         print('New record:')
-        logging.info(f'create: {eval(req_msg)}')
     print(eval(req_msg))
 
 
-def delete_record(record_id):
-    if record_id:
-        msg = httpx.delete(f'{base_url}/{record_id}', headers=headers)
-        if msg.json()['success']:
-            print('Deleted')
-            logging.info(f'delete: {record}')
+def delete_record(msgs):
+    if msgs:
+        option = int(input("choose the option: ")) if len(msgs) > 1 else 0
+        msg = msgs[option]
+        if httpx.delete(f'{base_url}/{msg["id"]}',
+                        headers=headers).json()['success']:
+            logger.info(f'DELETED | {eval(req_msg)}')
     else:
         print(f'Record not found: {name}')
 
@@ -57,11 +65,11 @@ def get_parser():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='/var/log/cloudflare_dns.log',
-                        format='%(levelname)s %(asctime)s %(message)s',
-                        level=logging.INFO)
     req_msg = """f'{msg["name"]} {msg["type"]} {msg["content"]}'"""
     base_url = f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records'
+    logger.remove(handler_id=None)
+    logger.add('/var/log/cloudflare_dns.log',
+               format="{time:YYYY-MM-DD HH:mm:ss} | {message}")
     headers = {
         'X-Auth-Email': cf_id,
         'X-Auth-Key': cf_key,
@@ -72,6 +80,7 @@ if __name__ == '__main__':
     content = args.content
     record_type = args.type
     delete = args.delete
+    multi = args.multi
     if name and delete:
         delete_record(list_record(name))
     elif name and content:
