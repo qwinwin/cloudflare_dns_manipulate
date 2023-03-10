62feca79c62c3536dcb357d40e3b4b7854503145
qwinwin <qwinwin7@gmail.com>
2023-03-10 12:33:53 +0800

    update

--- cloudflare_dns.py
+++ cloudflare_dns.py
@@ -4,15 +4,11 @@ import httpx
 import argparse
 from loguru import logger
 
-cf_id = "cloudflare account id(mail)"
-cf_key = "global api key"
-zone_id = "domain zone id"
-zone_name = 'domain zone name'
 
-
-def list_record(name=''):
-    req_name = f'{name}.{zone_name}'
+def list_record(name='', record_type=''):
+    req_name = f'{name}.{zone_name}' if name != '@' else f'{zone_name}'
     params = {'name': f'{req_name}'} if name else {'per_page': 100}
+    params['type'] = record_type
     resp = httpx.get(base_url, headers=headers, params=params)
     msgs = []
     count = 0
@@ -28,8 +24,13 @@ def list_record(name=''):
     return msgs
 
 
-def new_record(name, content, msgs, record_type='A'):
-    data = {'type': record_type, 'name': name, 'content': content}
+def new_record(name, content, msgs, record_type='', proxied=False):
+    data = {
+        'type': record_type,
+        'name': name,
+        'content': content,
+        'proxied': proxied
+    }
     if msgs and not multi:
         option = int(input("choose the option: ")) if len(msgs) > 1 else 0
         msg = msgs[option]
@@ -62,15 +63,25 @@ def get_parser():
         description="Manipulate DNS records hosted on cloudflare")
     parser.add_argument('-n', '--name')
     parser.add_argument('-c', '--content')
-    parser.add_argument('-t', '--type', default='A')
+    # parser.add_argument('-t', '--type', default='A')
+    parser.add_argument('-t', '--type')
+    parser.add_argument('-z', '--zone', default=0)
     parser.add_argument('-d', '--delete', action="store_true")
     parser.add_argument('-m', '--multi', action="store_true")
+    parser.add_argument('-p', '--proxied', action="store_true")
     return parser
 
 
 if __name__ == '__main__':
-    req_msg = """f'{msg["name"]} {msg["type"]} {msg["content"]}'"""
-    base_url = f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records'
+    cf_id = "cloudflare account id(mail)"
+    cf_key = "global api key"
+    zone_info = [{
+        'name': 'name1',
+        'id': 'xxxx'
+    }, {
+        'name': 'name2',
+        'id': 'xxxx'
+    }]
     logger.remove(handler_id=None)
     logger.add('/var/log/cloudflare_dns.log',
                format="{time:YYYY-MM-DD HH:mm:ss} | {message}")
@@ -85,9 +96,16 @@ if __name__ == '__main__':
     record_type = args.type
     delete = args.delete
     multi = args.multi
+    proxied = args.proxied
+    zone_num = int(args.zone)
+    zone_name, zone_id = zone_info[zone_num]['name'], zone_info[zone_num]['id']
+    req_msg = """f'{msg["name"]} {msg["type"]} {msg["content"]}'"""
+    base_url = f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records'
+
     if name and delete:
-        delete_record(list_record(name))
+        delete_record(list_record(name, record_type))
     elif name and content:
-        new_record(name, content, list_record(name), record_type)
+        new_record(name, content, list_record(name, record_type), record_type,
+                   proxied)
     else:
-        list_record(name)
+        list_record(name, record_type)
